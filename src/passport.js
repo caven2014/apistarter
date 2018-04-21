a9717e162c673b82fc78035ce37684fea7683e07
Pual <pual@liuxiaomaideMacBook-Pro.local>
2018-04-21 10:26:49 +0800

    init

---
+++ src/passport.js
@@ -0,0 +1,333 @@
+/**
+ * Copyright © 2016-present Kriasoft.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+/* @flow */
+/* eslint-disable no-param-reassign, no-underscore-dangle, max-len */
+
+import passport from 'passport';
+import {
+  Strategy as GoogleStrategy
+} from 'passport-google-oauth20';
+import {
+  Strategy as FacebookStrategy
+} from 'passport-facebook';
+import {
+  Strategy as TwitterStrategy
+} from 'passport-twitter';
+import {
+  Strategy as WechatStrategy
+} from 'passport-wechat';
+
+import db from './db';
+
+passport.serializeUser((user, done) => {
+  done(null, {
+    id: user.id,
+    displayName: user.displayName,
+    imageUrl: user.imageUrl,
+  });
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Creates or updates the external login credentials
+// and returns the currently authenticated user.
+async function login(req, provider, profile, tokens) {
+  let user;
+
+  if (req.user) {
+    user = await db
+      .table('users')
+      .where({
+        id: req.user.id
+      })
+      .first();
+  }
+  console.log('login function ===> ', profile)
+
+  if (!user) {
+    user = await db
+      .table('logins')
+      .innerJoin('users', 'users.id', 'logins.user_id')
+      .where({
+        'logins.provider': provider,
+        'logins.id': profile.id || profile.openid
+      })
+      .first('users.*');
+    if (!user &&
+      profile.emails &&
+      profile.emails.length &&
+      profile.emails[0].verified === true
+    ) {
+      user = await db
+        .table('users')
+        .innerJoin('emails', 'emails.user_id', 'users.id')
+        .where({
+          'emails.email': profile.emails[0].value,
+          'emails.verified': true,
+        })
+        .first('users.*');
+    }
+  }
+
+  if (!user) {
+    [user] = await db
+      .table('users')
+      .insert({
+        display_name: profile.displayName,
+        image_url: profile.photos && profile.photos.length ?
+          profile.photos[0].value : null,
+      })
+      .returning('*');
+
+    if (profile.emails && profile.emails.length) {
+      await db.table('emails').insert(
+        profile.emails.map(x => ({
+          user_id: user && user.id,
+          email: x.value,
+          verified: x.verified || false,
+        })),
+      );
+    }
+  }
+
+  const loginKeys = {
+    user_id: user.id,
+    provider,
+    id: profile.id
+  };
+  const {
+    count
+  } = await db
+    .table('logins')
+    .where(loginKeys)
+    .count('id')
+    .first();
+
+  if (count === '0') {
+    await db.table('logins').insert({
+      ...loginKeys,
+      username: profile.username,
+      tokens: JSON.stringify(tokens),
+      profile: JSON.stringify(profile._json),
+    });
+  } else {
+    await db
+      .table('logins')
+      .where(loginKeys)
+      .update({
+        username: profile.username,
+        tokens: JSON.stringify(tokens),
+        profile: JSON.stringify(profile._json),
+        updated_at: db.raw('CURRENT_TIMESTAMP'),
+      });
+  }
+
+  return {
+    id: user.id,
+    displayName: user.display_name,
+    imageUrl: user.image_url,
+  };
+}
+
+async function loginWechat(provider, profile, tokens) {
+
+  if (!user) {
+    user = await db
+      .table('logins')
+      .innerJoin('users', 'users.id', 'logins.user_id')
+      .where({
+        'logins.provider': provider,
+        'logins.id': profile.openid
+      })
+      .first('users.*');
+    if (!user &&
+      profile.openid &&
+      profile.nickname
+    ) {
+      user = await db
+        .table('users')
+        .innerJoin('wechat', 'wechat.open_id', 'wechat.nickname', 'users.id')
+        .where({
+          'wechat.open_id': profile.ipenid,
+          'wechat.nickname': profile.nickname
+        })
+        .first('users.*');
+    }
+  }
+
+  if (!user) {
+    [user] = await db
+      .table('users')
+      .insert({
+        display_name: profile.nickname,
+        image_url: profile.imageUrl || null
+      })
+      .returning('*');
+  }
+
+  const loginKeys = {
+    user_id: user.id,
+    provider,
+    id: profile.id
+  };
+  const {
+    count
+  } = await db
+    .table('logins')
+    .where(loginKeys)
+    .count('id')
+    .first();
+
+  if (count === '0') {
+    await db.table('logins').insert({
+      ...loginKeys,
+      username: profile.nickname,
+      tokens: JSON.stringify(tokens),
+      // profile: JSON.stringify(profile._json),
+    });
+  } else {
+    await db
+      .table('logins')
+      .where(loginKeys)
+      .update({
+        username: profile.username,
+        tokens: JSON.stringify(tokens),
+        profile: JSON.stringify(profile._json) || null,
+        updated_at: db.raw('CURRENT_TIMESTAMP'),
+      });
+  }
+
+  return {
+    id: user.id,
+    displayName: user.nickname,
+    imageUrl: user.image_url,
+  };
+}
+
+// https://www.npmjs.com/package/passport-wechat
+// https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc6e911cdded3962d&redirect_uri=http%3a%2f%2ff3934499.ngrok.io%2f&response_type=code&scope=snsapi_base&state=123#wechat_redirect
+passport.use(new WechatStrategy({
+    appID: 'wxc6e911cdded3962d',
+    name: 'wechat',
+    appSecret: '1ba02a7d9d1db6cbd10feb9936698b9d',
+    client: 'wechat', // wechat web
+    callbackURL: 'http://f3934499.ngrok.io/login/wechat/return',
+    scope: 'snsapi_userinfo', //{snsapi_userinfo|snsapi_base}
+    state: '123',
+    // getToken: {getToken},
+    // saveToken: {saveToken}
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    console.log(profile)
+    console.log(JSON.stringify(done))
+    try {
+      const user = await loginWechat(req, 'wechat', profile, {
+        accessToken,
+        refreshToken,
+      });
+      console.log(user)
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }
+));
+
+// https://github.com/jaredhanson/passport-google-oauth2
+passport.use(
+  new GoogleStrategy({
+      clientID: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: '/login/google/return',
+      passReqToCallback: true,
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
+      try {
+        const user = await login(req, 'google', profile, {
+          accessToken,
+          refreshToken,
+        });
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    },
+  ),
+);
+
+// https://github.com/jaredhanson/passport-facebook
+// https://developers.facebook.com/docs/facebook-login/permissions/
+passport.use(
+  new FacebookStrategy({
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      profileFields: [
+        'id',
+        'cover',
+        'name',
+        'age_range',
+        'link',
+        'gender',
+        'locale',
+        'picture',
+        'timezone',
+        'updated_time',
+        'verified',
+        'email',
+      ],
+      callbackURL: '/login/facebook/return',
+      passReqToCallback: true,
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
+      try {
+        if (profile.emails.length)
+          profile.emails[0].verified = !!profile._json.verified;
+        profile.displayName =
+          profile.displayName ||
+          `${profile.name.givenName} ${profile.name.familyName}`;
+        const user = await login(req, 'facebook', profile, {
+          accessToken,
+          refreshToken,
+        });
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    },
+  ),
+);
+
+// https://github.com/jaredhanson/passport-twitter
+passport.use(
+  new TwitterStrategy({
+      consumerKey: process.env.TWITTER_KEY,
+      consumerSecret: process.env.TWITTER_SECRET,
+      callbackURL: '/login/twitter/return',
+      includeEmail: true,
+      includeStatus: false,
+      passReqToCallback: true,
+    },
+    async (req, token, tokenSecret, profile, done) => {
+      try {
+        if (profile.emails && profile.emails.length)
+          profile.emails[0].verified = true;
+        const user = await login(req, 'twitter', profile, {
+          token,
+          tokenSecret,
+        });
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    },
+  ),
+);
+
+export default passport;
